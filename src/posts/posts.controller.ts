ad28c5f6147d14b063722af9b14cef32c1a6f4c0
ironPriest <iron.priest.mih@gmail.com>
2023-03-28 18:36:09 +0300

    #13 post creation endpoint

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,16 +1,29 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { PostsService } from './posts.service';
 import { PostsQueryRepository } from './repositories/posts-query.repository';
+import { PostInputDTO } from './types';
+import { BlogsQueryRepository } from '../blogs/repositories/blogs-query.repository';
 
 @Controller('posts')
 export class PostsController {
   constructor(
     protected postsService: PostsService,
     protected postsQueryRepository: PostsQueryRepository,
+    protected blogsQueryRepository: BlogsQueryRepository,
   ) {}
 
   @Get(':id')
   getPost(@Param('id') postId: string) {
+    return this.postsQueryRepository.getPost(postId);
+  }
+
+  @Post()
+  async createPost(
+    @Body() inputDTO: PostInputDTO,
+    @Body('blogId') blogId: string,
+  ) {
+    const blog = await this.blogsQueryRepository.getBlog(blogId);
+    const postId = await this.postsService.create(inputDTO, blogId, blog.name);
     return this.postsQueryRepository.getPost(postId);
   }
 }
